6b7273f9984697416c2606ec5ab2f8834a44d37d
Chris K <kaytwo@gmail.com>
2012-07-08 14:11:49 -0700

    italic kind of working, some corner cases still broken

--- dinocr.py
+++ dinocr.py
@@ -35,11 +35,18 @@ def print_prospective(pix):
 class Dinocr:
   charmap_file = open('charmap.pkl','rb')
   charmap_bold = open('charmap_bold.pkl','rb')
+  charmap_italic = open('charmap_italic.pkl','rb')
+
+  italic_charmap = pickle.load(charmap_italic)
+  italic_revmap = pickle.load(charmap_italic)
+
   bold_charmap = pickle.load(charmap_bold)
   bold_revmap = pickle.load(charmap_bold)
 
   charmap = pickle.load(charmap_file)
   revmap = pickle.load(charmap_file)
+
+  italic_offset = [3,3,3,2,2,2,1,1,1,0,0,-1,-1]
 
   def __init__(self,image):
     
@@ -87,11 +94,69 @@ class Dinocr:
       for x1 in range(9):
         self.pix[x+x1,y+y1] = 1
 
+  def erase_italic_character_area(self,x,y):
+    '''blank the 8x13 area starting at (x,y)'''
+    for y1 in range(13):
+      for x1 in range(8):
+        self.pix[x+x1 + Dinocr.italic_offset[y1],y+y1] = 1
+
   def erase_character_area(self,x,y):
     '''blank the 8x13 area starting at (x,y)'''
     for y1 in range(13):
       for x1 in range(8):
         self.pix[x+x1,y+y1] = 1
+
+  def match_with_italic_character(self,x,y,origin=False):
+    '''
+    with origin = true, (x,y) denotes the origin of where we expect
+    the character to be; 
+    with origin = false, (x,y) is the first 
+    colored character found, and we must find the prospective origin
+    to make the comparison
+    '''
+    if origin == True:
+      new_char = []
+      for y1 in range(13):
+        for x1 in range(8):
+          try:
+            new_char.append(self.pix[x + x1 + Dinocr.italic_offset[y1],
+                                   y + y1])
+          except KeyError:
+            new_char.append(1)
+      # print "testing aligned italic character:"
+      # printmap(new_char)
+      if tuple(new_char) in Dinocr.italic_revmap:
+        self.erase_italic_character_area(x,y)
+        return Dinocr.italic_revmap[tuple(new_char)],x,y
+      else:
+        return ' ',-1,-1
+    
+    # origin == False
+    for letter in Dinocr.italic_charmap.keys():
+      offsetx,offsety = Dinocr.italic_charmap[letter][1:]
+      letter_start_x = x - offsetx - Dinocr.italic_offset[offsety]
+      letter_start_y = y - offsety
+      if letter_start_x < 0 or letter_start_y < 0:
+        continue
+      # print("searching for exact match with %s at %d,%d" % 
+      #       (letter,letter_start_x,letter_start_y))
+      new_char = []
+      for y1 in range(13):
+        for x1 in range(8):
+          try:
+            new_char.append(self.pix[letter_start_x + x1 + Dinocr.italic_offset[y1],
+                              letter_start_y + y1])
+          except KeyError:
+            new_char.append(1)
+
+      # printmap(new_char)
+      if all([(new_char[a] > 0) == 
+              (Dinocr.italic_charmap[letter][0][a] > 0) 
+              for a in range(8*13)]):
+        # print "found character",letter
+        return letter, letter_start_x,letter_start_y
+    return '',-1,-1
+
 
   def match_with_character(self,x,y,origin=False):
     '''
@@ -187,6 +252,8 @@ class Dinocr:
       text_this_line = False
       while firstx  < self.sizex - 8:
         this_character = self.match_with_character(firstx,firsty,origin=True)[0]
+        if this_character == ' ':
+          this_character = self.match_with_italic_character(firstx,firsty,origin=True)[0]
         if this_character != '' and this_character != ' ':
           text_this_line = True
         line.append(this_character)
@@ -225,6 +292,10 @@ class Dinocr:
       if startx < 0:
         break
       letter, originx,originy = self.match_with_character(startx,starty)
+      if letter == '':
+        letter, originx,originy = self.match_with_italic_character(startx,starty)
+        if letter != '' and letter != ' ':
+          print "found italic letter",letter
       if letter != '':
         lines.append(self.find_aligned(originx,originy))
       letter, originx,originy = self.match_with_bold_character(startx,starty)
@@ -250,13 +321,14 @@ if __name__ == '__main__':
 
   out = Image.new('1',(735,500),255)
   im1 = Image.open("comic_mask.bmp")
-  im2 = Image.open("test_bold_italic.png")
+  im2 = Image.open("test_italic.png")
   im1 = im1.convert('1')
   im2 = im2.convert('1')
   out.paste(im2,None,im1)
   panels = []
-  for panel in cutpoints:
-    panels.append(out.crop(panel))
+  panels.append(out.crop(cutpoints[4]))
+  # for panel in cutpoints:
+  #   panels.append(out.crop(panel))
 
   for offset in range(len(panels)):
     this_panel = panels[offset]
